f6392f5fe38dab25eff616b9a1384cc5049882a4
samarasam24 <samarasamtmk2003@gmail.com>
2024-05-23 16:50:12 +0530

    redux-saga

--- src/components/CrudForm.js
+++ src/components/CrudForm.js
@@ -1,9 +1,21 @@
+import '../styles/CrudForm.css';
 export const CrudForm = () => {
 
     return(
-        <form>
-            <label>Name:</label>
-            <input/>
+        <form className='crudForm d-flex flex-column justify-content-center align-items-center'>
+           <div>
+              <label>Name:</label>
+              <br/>
+              <input className='rounded border '/>
+           </div>
+           <div>
+              <label>Password:</label>
+              <br/>
+              <input className='rounded border'/>
+           </div>
+           <div>
+              <button className='btn btn-primary mt-2'>Submit</button>
+           </div>
         </form>
     );
 };
