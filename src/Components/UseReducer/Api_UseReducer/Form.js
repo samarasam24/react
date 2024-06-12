7fabdcbc9831ed7697c3b3e0b9377908f96225ea
samarasam24 <samarasamtmk2003@gmail.com>
2024-06-12 16:52:02 +0530

    useReducer with api

--- src/Components/UseReducer/Api_UseReducer/Form.js
+++ src/Components/UseReducer/Api_UseReducer/Form.js
@@ -1,5 +1,39 @@
-export function UseReducerAPi(){
+import  TextField  from "@mui/material/TextField";
+import  Box  from "@mui/material/Box";
+import  shadows from "@mui/material/styles/shadows";
+import  Button  from "@mui/material/Button";
+import { useState } from "react";
+
+ 
+ export function UseReducerAPi(){
+
+    const [ user,setUser ] = useState(
+        {
+            userName:'',
+            userPassword:''
+        }
+    );
+    
     return(
-        <h1>APi</h1>
+       <> 
+       <Box 
+         component='form'
+         id="useReducerAPi" 
+         p={3} display={"flex"} 
+         flexDirection={"column"} 
+         position={"absolute"}
+         boxShadow={shadows}
+         borderRadius={1}
+         gap={2}
+         width={400} 
+         >
+        <Box component={'h4'}> UseReducer With-Api</Box>
+        <label>Name:</label>
+        <TextField size="small"/> 
+        <label>Password:</label>
+        <TextField size="small"/> 
+        <Box  alignSelf={'end'} ><Button variant="contained" type="submit">Submit</Button></Box>
+       </Box>
+       </>
     );
 };
--- src/Components/UseState/Api_UseState/Form.js
+++ src/Components/UseState/Api_UseState/Form.js
@@ -1,4 +1,4 @@
-import {  useEffect, useState } from "react";
+import {  useEffect } from "react";
 import {  apiGetByIdMethod, apiPostMethod, apiPutMethod } from "../../Api/MockApi";
 import {  useNavigate, useParams } from "react-router-dom";
 import * as Yup from 'yup';
